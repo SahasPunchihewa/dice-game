96df6cfd6bf21c0510ad44a8f2dcfeffebb5afe1
Sahas Punchihewa <sahasmcg2995@gmail.com>
2023-03-06 10:20:14 +0530

    Add about activity

---
+++ app/src/main/java/com/sahas/dicegame/AboutPopup.kt
@@ -0,0 +1,11 @@
+package com.sahas.dicegame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutPopup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_popup)
+    }
+}
--- app/src/main/java/com/sahas/dicegame/MainActivity.kt
+++ app/src/main/java/com/sahas/dicegame/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.sahas.dicegame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClickAbout(view: View) {
+        val aboutIntent = Intent(this, AboutPopup::class.java)
+        startActivity(aboutIntent)
     }
 }
