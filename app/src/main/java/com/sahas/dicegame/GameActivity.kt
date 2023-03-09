85e977992eec6cc1967fcc6f6b5829d92c557fbf
Sahas Punchihewa <sahasmcg2995@gmail.com>
2023-03-09 23:06:52 +0530

    Create basic layout of dice

--- app/src/main/java/com/sahas/dicegame/GameActivity.kt
+++ app/src/main/java/com/sahas/dicegame/GameActivity.kt
@@ -2,10 +2,31 @@ package com.sahas.dicegame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlin.random.Random
 
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+    }
+
+    fun onThrow(view: View) {
+        var player: List<Int> = generateNumbers()
+        var computer: List<Int> = generateNumbers()
+
+
+
+    }
+
+    fun onScore(view: View) {
+
+    }
+
+    fun generateNumbers(): List<Int> {
+        return List(5) {
+            (1..6).random()
+        }
     }
 }
