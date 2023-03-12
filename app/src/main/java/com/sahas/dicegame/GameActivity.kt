220118204243472105c4e49bf33cd97ff1c44450
Sahas Punchihewa <sahasmcg2995@gmail.com>
2023-03-12 21:49:20 +0530

    contraind layout change

--- app/src/main/java/com/sahas/dicegame/GameActivity.kt
+++ app/src/main/java/com/sahas/dicegame/GameActivity.kt
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
 import kotlin.random.Random
 
 class GameActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_game)
     }
 
